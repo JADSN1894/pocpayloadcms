ffb6cbc5ec759bc336f222e9837fe86bf30cb4cc
JADSN <120682244+JADSN1894@users.noreply.github.com>
2026-01-19 15:08:04 -0300

    Setup

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260119_174403 from './20260119_174403';
+
+export const migrations = [
+  {
+    up: migration_20260119_174403.up,
+    down: migration_20260119_174403.down,
+    name: '20260119_174403'
+  },
+];
